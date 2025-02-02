bd8b995dd2a62b6b835884f30ba1397181c43e1a
Lakshay <black.leopard0987@gmail.com>
2025-02-02 23:27:11 +0530

    Hello World

--- 0_HelloWorld.cpp
+++ 0_HelloWorld.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <iostream>                     //Preprocessor directive i.e.,#include-used to include header files i.e., iostream, cmath, etc -contain a set of predefined standard library functions
 using namespace std;
 
-int main(){
+int main(){                             //Execution of code begins from main function
     cout<<"Hello World"<<endl;
-    return 0;
+    return 0;                           //Exits status of a function
 }
