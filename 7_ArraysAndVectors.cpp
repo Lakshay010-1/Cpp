877cadde5e692160c8511813e16231e18f5450e9
Lakshay <black.leopard0987@gmail.com>
2025-02-07 23:53:04 +0530

    Array and Vectors

--- 7_ArraysAndVectors.cpp
+++ 7_ArraysAndVectors.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<array>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    /* Array */
     // Single-Dimensional Arrays
     int id[]={1,2,4,5,6,7,8,68};
     for(int i=0;i<sizeof(id)/sizeof(id[0]);i++){
@@ -18,6 +21,14 @@ int main(){
         }
         cout<<endl;
     }
+    /*STL Array */
+    array<int,5> stlArray={1,2,4,5,6};
+    int stlArrSize=stlArray.size();
+    int eleAt1=stlArray[1];
+    int eleAt2=stlArray.at(2);
+    bool isEmptyStl=stlArray.empty();
+    int firstStl=stlArray.front();
+    int lastStl=stlArray.back();
 
     /* Vectors (vector is like a resizable array.) */
     cout<<"Vectors"<<endl;
@@ -41,6 +52,26 @@ int main(){
     for(int i=0;i<length;i++){
         cout<<vec[i]<<" ";
     }
+
+    //Binary Search TC=0(logn) SC=0(1)
+    vector<int> elements={1,2,4,5,6,3,7};
+    sort(elements.begin(),elements.end());
+    int left=0,right=elements.size();
+    int targetIdx=-1, target;
+    cout<<"\nEnter element value to search in the array : "<<endl;
+    cin>>target;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(elements[mid]==target){
+            targetIdx=mid;
+            break;
+        }else if(elements[mid]>target){
+            right=mid-1;
+        }else{
+            left=mid+1;
+        }
+    }
+    cout<<"target index is: "<<targetIdx<<endl;
 
     return 0;
 }
