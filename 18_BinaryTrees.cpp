1040d92bbea32aac7a585faee6a880a8b1b019b7
Lakshay <black.leopard0987@gmail.com>
2025-02-22 18:04:25 +0530

    Binary Tree Implementation

--- 18_BinaryTrees.cpp
+++ 18_BinaryTrees.cpp
@@ -3,6 +3,8 @@
 #include<queue>
 using namespace std;
 
+//A binary tree is a tree-based hierarchical data structure where each node can have at most two children
+
 class BinaryTree{
     public:
     class Node{
@@ -32,6 +34,7 @@ class BinaryTree{
     Node* getRoot(){
         return root;
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
     Node* buildTree(Node *root){
         int val;
         cout<<"Enter element value (-1 for NULL) : "<<endl;
@@ -47,6 +50,7 @@ class BinaryTree{
         root->right=buildTree(root->right);
         return root;
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
     Node* buildTreePre(Node* root,vector<int> nodes,int &idx){
         if(idx>=nodes.size()){
             return NULL;
@@ -61,6 +65,7 @@ class BinaryTree{
         root->right=buildTreePre(root->right,nodes,idx);
         return root;
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
     Node* buildTreeLevel(Node* root){
         int val;
         queue<Node*> q;
@@ -88,6 +93,7 @@ class BinaryTree{
         return root;
         
     }
+    //Time Complexity=0(n), Space Complexity=0(1)
     void morrisTraversal(Node* root){
         // In-Order traversal
         Node* cur=root;
@@ -97,7 +103,7 @@ class BinaryTree{
                 cout<<cur->data<<" ";
                 cur=cur->right;
             }else{
-            // Find the inorder predecessor of curr
+                // Find the inorder predecessor of curr
                 Node* prev=cur->left;
                 while(prev->right!=nullptr && prev->right!=cur){
                     prev=prev->right;
@@ -107,7 +113,7 @@ class BinaryTree{
                     prev->right=cur;
                     cur=cur->left;
                 }else{
-                // Revert the changes made in the tree structure
+                    // Revert the changes made in the tree structure
                     prev->right=nullptr;
                     cout<<cur->data<<" ";
                     cur=cur->right;
@@ -115,6 +121,7 @@ class BinaryTree{
             }
         }
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
     void preOrderTraversal(Node* root){
         if(root==NULL){
             return;
@@ -123,6 +130,7 @@ class BinaryTree{
         preOrderTraversal(root->left);
         preOrderTraversal(root->right);
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
     void inOrderTraversal(Node *root){
         if(root==NULL){
             return;
@@ -130,8 +138,8 @@ class BinaryTree{
         inOrderTraversal(root->left);
         cout<<root->data<<" ";
         inOrderTraversal(root->right);
-
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
     void postOrderTraversal(Node *root){
         if(root==NULL){
             return;
@@ -140,6 +148,7 @@ class BinaryTree{
         postOrderTraversal(root->right);
         cout<<root->data<<" ";
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
     void levelOrderTraversal(Node *root){
         queue<Node*> q;
         q.push(root);
@@ -163,6 +172,23 @@ class BinaryTree{
         }
         cout<<endl;
     }
+    //Time Complexity=0(n), Space Complexity=0(n)
+    pair<bool,Node*> searchTarget(Node* root,int target){
+        pair<bool,Node*> res=make_pair(false,nullptr);
+        if(root==NULL){
+            return res;
+        }
+        if(root->data==target){
+            res=make_pair(true,root);
+            return res;
+        }
+        res=searchTarget(root->left,target);
+        if(res.first==false){
+            res=searchTarget(root->right,target);
+        }
+        return res;
+    }
+    //Time Complexity=0(n), Space Complexity=0(n)
     void deleteTree(Node* root){
         if(root==NULL){
             return;
@@ -177,13 +203,28 @@ class BinaryTree{
 
 int main(){
     BinaryTree *bt=new BinaryTree();
+    BinaryTree::Node* root=bt->getRoot();
+
+    ////Construct a binary tree.
+    //Approach-1
+    // root=bt->buildTree(root);
+    //Approach-2
+    // root=bt->buildTreeLevel(root);
+    //Approach-3
     vector<int> elements={1,2,4,-1,-1,-1,3,-1,5,-1,-1};
     int idx=0;
-    BinaryTree::Node* root=bt->getRoot();
-    // root=bt->buildTree(root);
-    // root=bt->buildTreeLevel(root);
     root=bt->buildTreePre(root,elements,idx);
     cout<<root->data<<endl;    
+    
+    //Search Target
+    int target=1;
+    pair<bool,BinaryTree::Node*> res=bt->searchTarget(root,target);
+    cout<<"\nis "<<target<<" present : "<<res.first<<endl;
+    if(res.first){
+        cout<<res.second->data<<endl;
+    }
+
+    //Traversals
     cout<<"\nMorris Traversal:"<<endl;
     bt->morrisTraversal(root);
     cout<<"\nLevel-Order Traversal:"<<endl;
@@ -196,7 +237,9 @@ int main(){
     bt->postOrderTraversal(root);
     cout<<endl;
 
+    //Freeing Memory
     bt->deleteTree(root);
     delete bt;
+
     return 0;
 }
