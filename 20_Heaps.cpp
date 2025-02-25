b704b39c13fef8afe1c9987b03c067b5ae488e41
Lakshay <black.leopard0987@gmail.com>
2025-02-25 16:27:26 +0530

    Heaps Implementation, Heap Sort

--- 20_Heaps.cpp
+++ 20_Heaps.cpp
@@ -9,12 +9,34 @@ using namespace std;
 // Max Heap = value of the node must be the greatest among all its descendant nodes
 // Min Heap = value of the node must be the smallest among all its descendant nodes
 
-class heap{
+template<typename K>
+class heap{                                         //Max Heap
     private:
     int size;
-    int* arr;
+    K* arr;
     int curPos;
     const int root=1;
+
+    // Returns parent index
+    // Time Complexity=0(1), Space Complexity=0(1)
+    int parent(int child){
+        return child/2;
+    }
+    
+    // Returns left child index
+    // Time Complexity=0(1), Space Complexity=0(1)
+    int leftChild(int parentIdx){
+        return parentIdx*2;
+    }
+
+    // Returns right child index
+    // Time Complexity=0(1), Space Complexity=0(1)
+    int rightChild(int parentIdx){
+        return (parentIdx*2)+1;
+    }
+
+    // heapifyUtil(idx) arranges the node at idx, and its subtrees accordingly so that the heap property is maintained.
+    // Time Complexity=0(logn), Space Complexity=0(logn)
     void heapifyUtil(int idx){
         int largestValIdx=idx;
         int left=leftChild(idx), right=rightChild(idx);
@@ -30,51 +52,50 @@ class heap{
             heapifyUtil(largestValIdx);
         }
     }
+
     public:
     heap(int size=50){
         this->size=size;
-        arr=new int[this->size];
+        arr=new K[this->size];
         curPos=0;
-        arr[root]=-1;
     }
-    heap(int arr[],int size){
+    heap(K arr[],int size){
         this->arr=arr;
-        size=size+10;
-        curPos=5;
+        size=size;
+        curPos=size;
     }
     ~heap(){
         delete [] arr;
         cout<<"Memory Freed"<<endl;
     }
-    // Return root index
-    int getRoot(){
-        return this->root;
-    }
+    
     //Insertion 
-    bool insertNodeMaxHeap(int val){
-        if(curPos+1>=size){
+    // Time Complexity=0(logn), Space Complexity=0(1)
+    bool insertNode(K val){
+        if(curPos>=size){
             return false;
         }
         int idx=++curPos;
         arr[idx]=val;
-        while(idx>0){
-            int parent=idx/2;
-            if(arr[idx]>arr[parent]){
-                swap(arr[parent],arr[idx]);
-                idx=parent;
+        while(idx>1){
+            int curParent=parent(idx);
+            if(arr[idx]>arr[curParent]){
+                swap(arr[curParent],arr[idx]);
+                idx=curParent;
             }else{
                 break;
             }
         }
         return true;
     }
+
     // Deletion
-    int deleteRoot(){
+    // Time Complexity=0(logn), Space Complexity=0(1)
+    K deleteRoot(){
         if(curPos<=0){
-            cout<<"heap is empty ";
             return INT_MIN;
         }
-        int deletedVal=arr[root];
+        K deletedVal=arr[root];
         arr[root]=arr[curPos--];
         int idx=root;
         while(idx<curPos){
@@ -96,79 +117,122 @@ class heap{
         }
         return deletedVal;
     }
-    // Returns left child
-    int leftChild(int parentIdx){
-        return parentIdx*2;
+
+    //Heap Sort
+    // Time Complexity=0(n.logn), Space Complexity=0(1).0(logn)=0(logn)
+    void heapSort(){
+        int tempCurPos=curPos;
+        while(curPos>1){
+            swap(arr[root],arr[curPos]);
+            curPos--;
+            heapifyUtil(root);
+        }
+        curPos=tempCurPos;
     }
-    // Returns right child
-    int rightChild(int parentIdx){
-        return (parentIdx*2)+1;
+
+    //Heapify
+    // Time Complexity=0(n.logn), Space Complexity=0(1).0(logn)=0(logn)
+    bool heapify(){
+        for(int i=curPos/2;i>=1;i--){
+            heapifyUtil(i);
+        }
+        return true;
     }
+
+    // Print Heap Leafs
+    // Time Complexity=0(n), Space Complexity=0(1)
+    void printLeafs(){
+        cout<<"Leafs : ";
+        for(int i=(curPos/2)+1;i<=curPos;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+
     // Print Heap
+    // Time Complexity=0(n), Space Complexity=0(1)
     void printHeap(){
+        cout<<"Heap : ";
         if(curPos<=0){
+            cout<<"Empty"<<endl;
             return;
         }
         for(int i=1;i<=curPos;i++){
             cout<<arr[i]<<" ";
         }
         cout<<endl;
-        cout<<"Curpos : "<<curPos<<endl;
-    }
-    // Print Heap Leafs
-    void printLeafs(){
-        for(int i=(curPos/2)+1;i<=curPos;i++){
-            cout<<arr[i]<<" ";
-        }
-        cout<<endl;
-    }
-    void heapify(){
-        for(int i=curPos/2;i>=1;i--){
-            heapifyUtil(i);
-        }
     }
 };
 
 
 int main(){
-    heap h;
-    h.insertNodeMaxHeap(1);
-    h.printHeap();
-    h.insertNodeMaxHeap(2);
-    h.printHeap();
-    h.insertNodeMaxHeap(3);
-    h.printHeap();
-    h.insertNodeMaxHeap(4);
-    h.printHeap();
-    h.insertNodeMaxHeap(5);
-    h.printHeap();
-    cout<<"Leafs are : ";
-    h.printLeafs();
+    heap<int> *h=new heap<int>();
+    cout<<"Insertion"<<endl;
+    h->insertNode(1);
+    h->printHeap();
+    h->insertNode(2);
+    h->printHeap();
+    h->insertNode(3);
+    h->printHeap();
+    h->insertNode(4);
+    h->printHeap();
+    h->insertNode(5);
+    h->printHeap();
+    h->printLeafs();
 
-    cout<<"Left Child "<<h.leftChild(1)<<endl;
-    cout<<"Right Child "<<h.rightChild(1)<<endl;
+    cout<<"\nDeletion "<<endl;
+    cout<<"Deleted Value : "<<h->deleteRoot()<<endl;
+    h->printHeap();
+    cout<<"Deleted Value : "<<h->deleteRoot()<<endl;
+    h->printHeap();
+    cout<<"Deleted Value : "<<h->deleteRoot()<<endl;
+    h->printHeap();
+    cout<<"Deleted Value : "<<h->deleteRoot()<<endl;
+    h->printHeap();
+    cout<<"Deleted Value : "<<h->deleteRoot()<<endl;
+    h->printHeap();
+    cout<<"Deleted Value : "<<h->deleteRoot()<<endl;
+    h->printHeap();
 
-    // cout<<"Deleted Root is : "<<h.deleteRoot()<<endl;
-    // h.printHeap();
-    // cout<<"Deleted Root is : "<<h.deleteRoot()<<endl;
-    // h.printHeap();
-    // cout<<"Deleted Root is : "<<h.deleteRoot()<<endl;
-    // h.printHeap();
-    // cout<<"Deleted Root is : "<<h.deleteRoot()<<endl;
-    // h.printHeap();
-    // cout<<"Deleted Root is : "<<h.deleteRoot()<<endl;
-    // h.printHeap();
-    // cout<<"Deleted Root is : "<<h.deleteRoot()<<endl;   
-    // h.printHeap();
+    int elements[]={-1,1,2,5,4,3};
+    h=new heap<int>(elements,5);
+    cout<<endl<<"New Elements : ";
+    for(int i=1;i<sizeof(elements)/sizeof(elements[0]);i++){
+        cout<<elements[i]<<" ";
+    }
+    cout<<endl;
 
-    h.heapify();
-    h.printHeap();
+    cout<<"\nHeapify-ing "<<endl;
+    h->heapify();
+    h->printHeap();
 
-    int maxheap[]={-1,1,2,3,4,5};
-    heap h2(maxheap,5);
-    h2.printHeap();
-    h2.heapify();
-    h2.printHeap();
+    cout<<"\nHeap Sort "<<endl;
+    h->heapSort();
+    h->printHeap();    
     
+    cout<<"\nHeapify-ing "<<endl;
+    h->heapify();
+    h->printHeap();
+    cout<<endl;
+    
+    //Character map heap
+    // cout<<endl;
+    // heap<char> *hChar=new heap<char>();
+    // cout<<"Insertion"<<endl;
+    // hChar->insertNode('a');
+    // hChar->printHeap();
+    // hChar->insertNode('b');
+    // hChar->printHeap();
+    // hChar->insertNode('c');
+    // hChar->printHeap();
+    // hChar->insertNode('d');
+    // hChar->printHeap();
+    // hChar->insertNode('e');
+    // hChar->printHeap();
+    
+    //Freeing Memory
+    delete h;
+    // delete hChar;
+
     return 0;
 }
